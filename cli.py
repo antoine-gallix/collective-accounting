b9b8bfd92f3709ed72a64c7871a2a4a5dc1f912f
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-08-14 17:03:21 +0200

    color ledger table

--- cli.py
+++ cli.py
@@ -2,6 +2,9 @@ import click
 import collective_accounting
 from collective_accounting import logger
 import sys
+from rich import print
+from rich.table import Table
+from rich.text import Text
 
 main = click.Group()
 
@@ -14,12 +17,25 @@ def load_group():
         sys.exit()
 
 
+def format_credit(credit):
+    formated = Text(f"{credit:+}")
+    if credit > 0:
+        formated.style = "green"
+    elif credit < 0:
+        formated.style = "red"
+    return formated
+
+
 @main.command
 def show():
     """Show ledger in it's current state"""
     group = load_group()
+    table = Table(title="Ledger")
+    table.add_column("Account")
+    table.add_column("Balance")
     for account in group.accounts:
-        print(account)
+        table.add_row(account.name, format_credit(account.credit))
+    print(table)
 
 
 if __name__ == "__main__":
