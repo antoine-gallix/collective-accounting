8699255df0b407a0468035632b495f006302d2c3
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-08-14 16:07:22 +0200

    cli show command

--- cli.py
+++ cli.py
@@ -1,11 +1,21 @@
 import click
+import collective_accounting
+from collective_accounting import logger
+import sys
 
 main = click.Group()
 
 
 @main.command
-def do_this():
-    pass
+def show():
+    """Show ledger in it's current state"""
+    try:
+        group = collective_accounting.Group.import_()
+    except FileNotFoundError as e:
+        logger.error(e)
+        sys.exit()
+    for account in group.accounts:
+        print(account)
 
 
 if __name__ == "__main__":
