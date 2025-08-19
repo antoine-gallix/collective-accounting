218d5b29894c1919d0f30309c58dd0046ff6c953
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-08-19 13:56:33 +0200

    fix typo

--- tests/test_models.py
+++ tests/test_models.py
@@ -73,7 +73,7 @@ def test__Ledger__file_IO(ledger, mocker, tmp_path):
 def test__Ledger__context_manager(saved_ledger):
     assert saved_ledger.as_dict() == {"antoine": 8.0, "baptiste": -4.0, "renan": -4.0}
     with Ledger.edit() as managed_ledger:
-        managed_ledger.recor_shared_expense(amount=21, by="antoine")
+        managed_ledger.record_shared_expense(amount=21, by="antoine")
     loaded_ledger = Ledger.load_from_file()
     assert loaded_ledger.as_dict() == {
         "antoine": 22.0,
