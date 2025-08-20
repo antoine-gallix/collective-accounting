470f296eb05293c3fe0f63dc3bdf3aa254061d74
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-08-20 18:58:10 +0200

    fix test and add test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,11 @@
 from decimal import Decimal
 
-from collective_accounting.utils import divide, file_creation_timestamp, round_to_cent
+from collective_accounting.utils import (
+    divide,
+    file_creation_timestamp,
+    file_modification_timestamp,
+    round_to_cent,
+)
 
 # ------------------------ decimal ------------------------
 
@@ -19,17 +24,32 @@ def test__divide():
 # ------------------------ file ------------------------
 
 
-def test__timestamp(tmp_path):
+def test__modification_timestamp(tmp_path):
     file = tmp_path / "file"
     # no file
-    assert file_creation_timestamp(file) is None
+    assert file_modification_timestamp(file) is None
     # timestamp is a stable number
     file.write_text("something")
-    first_timestamp = file_creation_timestamp(file)
+    first_timestamp = file_modification_timestamp(file)
     assert first_timestamp is not None
     assert isinstance(first_timestamp, float)
-    assert file_creation_timestamp(file) == first_timestamp
+    assert file_modification_timestamp(file) == first_timestamp
     # timestamp changes after a write
     file.write_text("something more")
-    second_timestamp = file_creation_timestamp(file)
+    second_timestamp = file_modification_timestamp(file)
     assert second_timestamp > first_timestamp
+
+
+def test__creation_timestamp(tmp_path):
+    file = tmp_path / "file"
+    # no file: None
+    assert file_creation_timestamp(file) is None
+    # indicates creation time
+    file.write_text("something")
+    first_timestamp = file_creation_timestamp(file)
+    assert isinstance(first_timestamp, float)
+    # do not change over time
+    assert file_creation_timestamp(file) == first_timestamp
+    # ... even after a update
+    file.write_text("something more")
+    assert file_creation_timestamp(file) == first_timestamp
