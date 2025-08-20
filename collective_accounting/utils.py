8c5d849dc733811fd3f451d09ca4683b1d31d184
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-08-20 16:03:40 +0200

    display layout

--- collective_accounting/utils.py
+++ collective_accounting/utils.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 
 import arrow
 import funcy
+from rich.align import Align
 from rich.layout import Layout
 from rich.panel import Panel
 from rich.table import Table
@@ -52,16 +53,24 @@ def format_timestamp(timestamp):
     return arrow.get(timestamp).to("local").format("YYYY-MM-DD HH:mm:ss")
 
 
-def make_header(ledger):
-    return Text(
-        "\n".join(
-            [
-                f"file: {Ledger.LEDGER_FILE}",
-                f"last update: {format_timestamp(timestamp(Ledger.LEDGER_FILE))}",
-            ]
-        ),
-        justify="center",
-    )
+def make_file_view(ledger):
+    elements = [
+        f"file: {Ledger.LEDGER_FILE}",
+        f"creation: {format_timestamp(file_creation_timestamp(Ledger.LEDGER_FILE))}",
+        f"last update: {format_timestamp(file_modification_timestamp(Ledger.LEDGER_FILE))}",
+    ]
+    view = Layout()
+    view.split_row(*[Layout(Text(element, justify="center")) for element in elements])
+    return view
+
+
+def make_ledger_view(ledger):
+    table = Table()
+    table.add_column("Account")
+    table.add_column("Balance")
+    for account in ledger.accounts:
+        table.add_row(account.name, format_balance(account.balance))
+    return table
 
 
 def build_ledger_view():
@@ -70,14 +79,14 @@ def build_ledger_view():
     except FileNotFoundError:
         return Text("no ledger file", style="red")
 
-    table = Table()
-    table.add_column("Account")
-    table.add_column("Balance")
-    for account in ledger.accounts:
-        table.add_row(account.name, format_balance(account.balance))
-
+    file_view = make_file_view(ledger)
+    ledger_view = make_ledger_view(ledger)
     layout = Layout()
     layout.split_column(
-        Layout(Panel(make_header(ledger))), Layout(Panel(table), ratio=5)
+        Layout(Panel(file_view, title="file"), size=3),
+        Layout(
+            Panel(Align(ledger_view, align="center", vertical="middle"), title="ledger")
+        ),
     )
+
     return layout
