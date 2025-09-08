8d51ff9a98ddd3cad648721637584741becfd3cb
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-09-08 17:08:47 +0200

    remove test ledger script

--- make_test_ledger.fish
+++
@@ -1,9 +0,0 @@
-rm ledger.pkl
-uv run accountant init
-uv run accountant add-user antoine
-uv run accountant add-user renan
-uv run accountant add-user baptiste
-uv run accountant record-shared-expense 25 antoine
-uv run accountant record-shared-expense 8 renan
-uv run accountant record-shared-expense 47 baptiste
-uv run accountant record-transfer 12 antoine baptiste
