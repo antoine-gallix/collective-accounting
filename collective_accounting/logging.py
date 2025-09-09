b73a2a802edd5cb1bc977580744fff19123b9185
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-09-09 16:31:38 +0200

    log level override from env var

--- collective_accounting/logging.py
+++ collective_accounting/logging.py
@@ -1,6 +1,12 @@
+import os
 import sys
 
 from loguru import logger
 
 logger.remove()
-logger.add(sys.stdout, format="<level>{message}</level>", colorize=True, level="INFO")
+logger.add(
+    sys.stdout,
+    format="<level>{message}</level>",
+    colorize=True,
+    level=os.environ.get("LEVEL") or "INFO",
+)
