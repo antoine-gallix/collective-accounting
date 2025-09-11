7e735157807d9e41d0f887ecd1f8ca9032f7ca7d
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-09-11 04:01:39 +0200

    ledger state

--- collective_accounting/account.py
+++ collective_accounting/account.py
@@ -32,3 +32,58 @@ class PositiveAccount(Account):
         if -amount > self.balance:
             raise RuntimeError("account balance cannot be negative")
         super().change_balance(amount)
+
+
+class LedgerState(dict[str, Account]):
+    """A collection of accounts with a balance. Represents the state of a ledger at a given point in time.
+
+    Operations on a ledger state are:
+    - addition and removal of accounts
+    - application of balanced changes to accounts
+
+    LedgerState will raise Exceptions in the following cases:
+    - add an account with name that is not a string
+    - add an account with name that is empty
+    - add an account with name that already exists
+    - remove an account that does not exist
+    - remove an account that does not have a null balance
+    """
+
+    @classmethod
+    def _validate_name(cls, name):
+        if not isinstance(name, str):
+            raise TypeError("name is not a string")
+        if not name:
+            raise ValueError("name string is empty")
+
+    def add_account(self, name: str):
+        self._validate_name(name)
+        if name in self:
+            raise RuntimeError("account already exists")
+        self[name] = Account()
+
+    def remove_account(self, name: str):
+        try:
+            if not self[name].is_settled:
+                raise RuntimeError("account cannot be removed if not settled")
+            del self[name]
+        except KeyError:
+            raise RuntimeError("account does not exists")
+
+    def change_balance(self, name: str, amount: Money):
+        logger.debug(f"balance change: {name} {amount!s}")
+        try:
+            self[name].change_balance(amount)
+        except KeyError:
+            raise RuntimeError("account does not exists")
+
+    def change_diff(self, name: str, amount: Money):
+        logger.debug(f"difference change: {name} {amount!s}")
+        try:
+            self[name].expect(amount)
+        except KeyError:
+            raise RuntimeError("account does not exists")
+
+    def check_equilibrium(self):
+        if (error := sum(account.diff for account in self.values())) != 0:
+            raise RuntimeError(f"accounts not equilibrated. Sum of diffs is {error:+}")
--- tests/test_account.py
+++ tests/test_account.py
@@ -1,17 +1,35 @@
 from pytest import fixture, raises
 
-from collective_accounting.account import Account, PositiveAccount
+from collective_accounting.account import Account, LedgerState, PositiveAccount
 from collective_accounting.money import Money
-
-
-def test__Account__defaults():
-    account = Account()
-    assert account == Account(balance=Money(0), diff=Money(0))
 
 
 @fixture
 def account():
     return Account()
+
+
+@fixture
+def ledger_state():
+    ledger_state = LedgerState()
+    ledger_state.add_account("antoine")
+    ledger_state.add_account("baptiste")
+    ledger_state.add_account("renan")
+    return ledger_state
+
+
+@fixture
+def ledger_state_with_pot(ledger_state):
+    ledger_state.add_account("POT")
+    return ledger_state
+
+
+# ------------------------ Account ------------------------
+
+
+def test__Account__defaults():
+    account = Account()
+    assert account == Account(balance=Money(0), diff=Money(0))
 
 
 def test__Account__expect(account):
@@ -45,3 +63,104 @@ def test__PositiveAccount():
     with raises(RuntimeError):
         account.change_balance(Money(-10))
     assert account == PositiveAccount(balance=Money(0), diff=Money(0))
+
+
+# ------------------------ LedgerState ------------------------
+
+
+def test__LedgerState__creation():
+    state = LedgerState()
+    assert state == {}
+
+
+# --------
+
+
+def test__LedgerState__add_account():
+    state = LedgerState()
+    state.add_account("antoine")
+    assert state == {"antoine": Account(balance=Money(0), diff=Money(0))}
+
+
+def test__LedgerState__add_account__invalid():
+    state = LedgerState()
+    # not a string
+    with raises(TypeError):
+        state.add_account(12)  # type: ignore
+    # empty string
+    with raises(ValueError):
+        state.add_account("")
+
+
+def test__LedgerState__add_account__existing_name(ledger_state):
+    with raises(RuntimeError):
+        ledger_state.add_account("antoine")
+
+
+# --------
+
+
+def test__LedgerState__remove_account(ledger_state):
+    ledger_state.remove_account("antoine")
+    assert ledger_state == {
+        "baptiste": Account(balance=Money(0), diff=Money(0)),
+        "renan": Account(balance=Money(0), diff=Money(0)),
+    }
+
+
+def test__LedgerState__remove_account__not_exist(ledger_state):
+    with raises(RuntimeError):
+        ledger_state.remove_account("kriti")
+
+
+def test__LedgerState__remove_account__non_null_balance(ledger_state):
+    ledger_state.change_balance("antoine", 10)
+    with raises(RuntimeError):
+        ledger_state.remove_account("antoine")
+
+
+# --------
+
+
+def test__LedgerState__change_balance(ledger_state):
+    ledger_state.change_balance("antoine", 10)
+    assert ledger_state == {
+        "antoine": Account(balance=Money(10), diff=Money(-10)),
+        "baptiste": Account(balance=Money(0), diff=Money(0)),
+        "renan": Account(balance=Money(0), diff=Money(0)),
+    }
+
+
+def test__LedgerState__change_balance__name_do_not_exist(ledger_state):
+    with raises(RuntimeError):
+        ledger_state.change_balance("kriti", Money(10))
+
+
+# --------
+
+
+def test__LedgerState__change_diff(ledger_state):
+    ledger_state.change_diff("antoine", 10)
+    assert ledger_state == {
+        "antoine": Account(balance=Money(0), diff=Money(10)),
+        "baptiste": Account(balance=Money(0), diff=Money(0)),
+        "renan": Account(balance=Money(0), diff=Money(0)),
+    }
+
+
+def test__LedgerState__change_diff__name_do_not_exist(ledger_state):
+    with raises(RuntimeError):
+        ledger_state.change_diff("kriti", Money(10))
+
+
+# --------
+
+
+def test__LedgerState__check_equilibriumbb(ledger_state):
+    ledger_state.check_equilibrium()
+    ledger_state.change_diff("antoine", Money(10))
+    with raises(RuntimeError):
+        ledger_state.check_equilibrium()
+    ledger_state.change_diff("renan", Money(-5))
+    ledger_state.change_diff("baptiste", Money(-5))
+    ledger_state.check_equilibrium()
