307a50da909bb078fd014459bd49b5f589697f86
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-09-13 01:53:01 +0200

    ledger high level operations

---
+++ collective_accounting/ledger.py
@@ -0,0 +1,119 @@
+from copy import copy
+from dataclasses import dataclass, field
+
+import funcy
+
+from .account import LedgerState
+from .logging import logger
+from .money import Money
+from .operations import (
+    AddAccount,
+    AddPot,
+    Operation,
+    PaysContribution,
+    Reimburse,
+    RequestContribution,
+    SharedExpense,
+    Transfer,
+    TransferDebt,
+)
+
+
+@dataclass
+class LedgerRecord:
+    state: LedgerState
+    operation: Operation
+
+
+@dataclass
+class Ledger:
+    records: list[LedgerRecord] = field(default_factory=list)
+    LEDGER_FILE = "ledger.yml"
+
+    @property
+    def state(self):
+        if not self.records:
+            return LedgerState()
+        else:
+            return self.records[-1].state
+
+    @property
+    def operations(self):
+        return [record.operation for record in self.records]
+
+    # ------------------------ IOs ------------------------
+
+    # def save_to_file(self):
+    #     operations_as_dicts = funcy.map(
+    #         operation_as_dict, funcy.pluck_attr("operation", self.records)
+    #     )
+    #     pathlib.Path(self.LEDGER_FILE).write_text(
+    #         yaml.dump_all(
+    #             operations_as_dicts,
+    #             sort_keys=False,
+    #         )
+    #     )
+
+    # @classmethod
+    # def load_from_file(cls) -> Self:
+    #     logger.debug(f"load operations from file: {cls.LEDGER_FILE}")
+    #     operation_dicts = yaml.load_all(
+    #         pathlib.Path(cls.LEDGER_FILE).read_text(), Loader=yaml.Loader
+    #     )
+    #     operations = funcy.map(load_operation_from_dict, operation_dicts)
+    #     logger.debug("replay operations")
+    #     ledger = cls()
+    #     for operation in operations:
+    #         ledger.apply(operation)
+    #     logger.debug("ledger loaded")
+    #     return ledger
+
+    # @classmethod
+    # @contextmanager  # type: ignore
+    # def edit(cls) -> Self:  # type: ignore
+    #     ledger = cls.load_from_file()
+    #     yield ledger  # type: ignore
+    #     ledger.save_to_file()
+
+    # ------------------------ record ------------------------
+
+    def record(self, operation):
+        logger.debug(f"record operation: {operation}")
+        try:
+            new_state = copy(self.state)
+            operation.apply_to(new_state)
+            new_state.check_equilibrium()
+        except:
+            logger.error("operation could not been applied")
+            raise
+        self.records.append(LedgerRecord(operation=operation, state=new_state))
+
+    # ------------------------ convenience ------------------------
+
+    def add_account(self, name):
+        self.record(AddAccount(name))
+
+    def add_pot(self):
+        self.record(AddPot())
+
+    def record_shared_expense(self, amount, name, subject):
+        self.record(SharedExpense(Money(amount), name, subject))
+
+    def record_transfer(self, amount, sender, receiver):
+        self.record(Transfer(amount=Money(amount), sender=sender, receiver=receiver))
+
+    def record_transfer_debt(self, amount, old_debitor, new_debitor):
+        self.record(
+            TransferDebt(
+                amount=Money(amount), old_debitor=old_debitor, new_debitor=new_debitor
+            )
+        )
+
+    def request_contribution(self, amount):
+        self.record(RequestContribution(Money(amount)))
+
+    def pays_contribution(self, amount, by):
+        self.record(PaysContribution(Money(amount), by))
+
+    def reimburse(self, amount, to):
+        self.record(Reimburse(Money(amount), to))
---
+++ tests/test_ledger.py
@@ -0,0 +1,130 @@
+from pytest import fixture
+
+from collective_accounting.account import Account, PositiveAccount
+from collective_accounting.ledger import Ledger
+from collective_accounting.money import Money
+from collective_accounting.operations import (
+    AddAccount,
+    AddPot,
+    PaysContribution,
+    Reimburse,
+    RequestContribution,
+    SharedExpense,
+    Transfer,
+    TransferDebt,
+)
+
+
+@fixture
+def ledger():
+    ledger = Ledger()
+    ledger.add_account("antoine")
+    ledger.add_account("baptiste")
+    ledger.add_account("renan")
+    return ledger
+
+
+# --------
+
+
+def test__Ledger__create():
+    ledger = Ledger()
+    assert ledger.state == {}
+
+
+def test__Ledger__scenario__populate_ledger():
+    ledger = Ledger()
+    ledger.add_account("antoine")
+    ledger.add_account("baptiste")
+    ledger.add_account("renan")
+    assert ledger.operations == [
+        AddAccount(name="antoine"),
+        AddAccount(name="baptiste"),
+        AddAccount(name="renan"),
+    ]
+    assert ledger.state == {
+        "antoine": Account(balance=Money("0.00"), diff=Money("0.00")),
+        "baptiste": Account(balance=Money("0.00"), diff=Money("0.00")),
+        "renan": Account(balance=Money("0.00"), diff=Money("0.00")),
+    }
+
+
+def test__Ledger__scenario__shared_expense(ledger):
+    ledger.record_shared_expense(amount=125, name="antoine", subject="potatoes")
+    ledger.record_transfer(
+        amount=30,
+        sender="baptiste",
+        receiver="antoine",
+    )
+    ledger.record_transfer_debt(40, "renan", "baptiste")
+    assert ledger.operations == [
+        AddAccount(name="antoine"),
+        AddAccount(name="baptiste"),
+        AddAccount(name="renan"),
+        SharedExpense(
+            amount=Money("125.00"),
+            payer="antoine",
+            subject="potatoes",
+        ),
+        Transfer(
+            amount=Money("30.00"),
+            sender="baptiste",
+            receiver="antoine",
+        ),
+        TransferDebt(
+            amount=Money("40.00"),
+            old_debitor="renan",
+            new_debitor="baptiste",
+        ),
+    ]
+    assert ledger.state == {
+        "antoine": Account(balance=Money("-95.00"), diff=Money("53.34")),
+        "baptiste": Account(balance=Money("-30.00"), diff=Money("-51.67")),
+        "renan": Account(balance=Money("0.00"), diff=Money("-1.67")),
+    }
+
+
+def test__Ledger__scenario__pot(ledger):
+    ledger.add_pot()
+    ledger.request_contribution(50)
+    ledger.pays_contribution(50, "antoine")
+    ledger.pays_contribution(30, "baptiste")
+    ledger.pays_contribution(50, "renan")
+    ledger.record_shared_expense(amount=125, name="antoine", subject="potatoes")
+    ledger.reimburse(100, "antoine")
+    assert ledger.operations == [
+        AddAccount(name="antoine"),
+        AddAccount(name="baptiste"),
+        AddAccount(name="renan"),
+        AddPot(),
+        RequestContribution(
+            amount=Money("50.00"),
+        ),
+        PaysContribution(
+            amount=Money("50.00"),
+            sender="antoine",
+        ),
+        PaysContribution(
+            amount=Money("30.00"),
+            sender="baptiste",
+        ),
+        PaysContribution(
+            amount=Money("50.00"),
+            sender="renan",
+        ),
+        SharedExpense(
+            amount=Money("125.00"),
+            payer="antoine",
+            subject="potatoes",
+        ),
+        Reimburse(
+            amount=Money("100.00"),
+            receiver="antoine",
+        ),
+    ]
+    assert ledger.state == {
+        "antoine": Account(balance=Money("-75.00"), diff=Money("25.00")),
+        "baptiste": Account(balance=Money("-30.00"), diff=Money("-20.00")),
+        "renan": Account(balance=Money("-50.00"), diff=Money("0.00")),
+        "POT": PositiveAccount(balance=Money("30.00"), diff=Money("-5.00")),
+    }
