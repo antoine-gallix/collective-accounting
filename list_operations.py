99b4e95e4727cf4a987fecd8071af9e939387e73
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-09-18 14:11:37 +0200

    operation list detailed

--- list_operations.py
+++ list_operations.py
@@ -5,11 +5,30 @@ import funcy
 
 from collective_accounting import operations
 
-operations_objects = (getattr(operations, name) for name in dir(operations))
-operations_classes = funcy.filter(
-    funcy.all_fn(inspect.isclass, funcy.rpartial(issubclass, operations.Operation)),
+
+def module_objects(module):
+    return [getattr(operations, name) for name in dir(operations)]
+
+
+def is_subclass_of(parent_class):
+    return funcy.all_fn(
+        inspect.isclass,
+        funcy.rpartial(issubclass, parent_class),
+        lambda x: x is not parent_class,
+    )
+
+
+operations_objects = module_objects(operations)
+account_operation_classes = funcy.filter(
+    is_subclass_of(operations.AccountOperation),
     operations_objects,
 )
-operations_names = funcy.map(attrgetter("__name__"), operations_classes)
+accounting_operation_classes = funcy.filter(
+    is_subclass_of(operations.AccountingOperation),
+    operations_objects,
+)
 
-print("\n".join(list(operations_names)))
+print("--- Account Operations ---")
+print("\n".join(list(funcy.map(attrgetter("__name__"), account_operation_classes))))
+print("--- Accounting Operations ---")
+print("\n".join(list(funcy.map(attrgetter("__name__"), accounting_operation_classes))))
